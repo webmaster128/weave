defef72b2b9c3953b4ed376d6de6194ff8563e4f
Alex Peters <alpe@users.noreply.github.com>
2018-09-19 12:06:33 +0200

    Add issue handler and validation

--- x/nft/approvals.go
+++ x/nft/approvals.go
@@ -13,3 +13,38 @@ func (i *ActionApprovals) Validate() error {
 	// todo: do proper validation
 	return nil
 }
+
+func (i *ActionApprovals) Clone() *ActionApprovals {
+	if i == nil {
+		return nil
+	}
+	approvals := make([]*Approval, len(i.Approvals))
+	for i, a := range i.Approvals {
+		approvals[i] = a.Clone()
+	}
+	return &ActionApprovals{
+		Action:    i.Action,
+		Approvals: approvals,
+	}
+}
+
+func (i *Approval) Clone() *Approval {
+	if i == nil {
+		return nil
+	}
+	return &Approval{
+		Address: i.Address,
+		Options: i.Options.Clone(),
+	}
+}
+
+func (i *ApprovalOptions) Clone() *ApprovalOptions {
+	if i == nil {
+		return nil
+	}
+	return &ApprovalOptions{
+		UntilBlockHeight: i.UntilBlockHeight,
+		Count:            i.Count,
+		Immutable:        i.Immutable,
+	}
+}
